40ecada43ffc9cd8891214d91ee077dbed899055
Kofi Brandful <rhutaford@gmail.com>
2025-08-14 12:53:56 +0000

    improved the code using with clause

--- pytopost.py
+++ pytopost.py
@@ -12,59 +12,54 @@ port_id = 5432
 conn = None
 curs = None
 try:
-    conn = psycopg2.connect(
+    with psycopg2.connect(
         host = hostname,
         dbname = database,
         user = username,
         password = pwd,
         port = port_id
-    )
+    ) as conn:
 
-    curs = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
     
-    # drop table if it already exists
-    curs.execute('DROP TABLE IF EXISTS employee')
+            # drop table if it already exists
+            curs.execute('DROP TABLE IF EXISTS employee')
 
-    create_script = ''' CREATE TABLE IF NOT EXISTS employee (
-        id int PRIMARY KEY,
-        name varchar(50) NOT NULL,
-        salary INT,
-        dept_id Varchar(30)
-    )
-'''
-    curs.execute(create_script)
+            create_script = ''' CREATE TABLE IF NOT EXISTS employee (
+                id int PRIMARY KEY,
+                name varchar(50) NOT NULL,
+                salary INT,
+                dept_id Varchar(30)
+            )
+        '''
+            curs.execute(create_script)
 
-    insert_scipt = 'INSERT INTO employee(id, name, salary, dept_id) values (%s, %s, %s, %s)'
-    insert_values =[(1,'Point', 27000, 'mapping dept'), (2, 'Stun', 15000, 'geology_dept'), 
-                    (3, 'Kofi',14000,'gis_dept'), (4, 'Range', 25000,'mapping'), (5, 'King',13000, 'mine_dept')]
-    for value in insert_values:
-        curs.execute(insert_scipt,value)
+            insert_scipt = 'INSERT INTO employee(id, name, salary, dept_id) values (%s, %s, %s, %s)'
+            insert_values =[(1,'Point', 27000, 'mapping dept'), (2, 'Stun', 15000, 'geology_dept'), 
+                            (3, 'Kofi',14000,'gis_dept'), (4, 'Range', 25000,'mapping'), (5, 'King',13000, 'mine_dept')]
+            for value in insert_values:
+                curs.execute(insert_scipt,value)
 
-# update the record
-    update_script = 'UPDATE employee SET salary = salary + (salary * 0.5)'
-    curs.execute(update_script)   
+        # update the record
+            update_script = 'UPDATE employee SET salary = salary + (salary * 0.5)'
+            curs.execute(update_script)   
 
-    # delete record
-    delete_script = 'DELETE FROM employee WHERE  name = %s'
-    delete_value = ('Stun',)
-    curs.execute(delete_script,delete_value)
-
-
-    # view items in table each on it's own line
-    curs.execute('SELECT * FROM employee')
-    for record in curs.fetchall():
-        print(record[1], record[2])
-
-    
+            # delete record
+            delete_script = 'DELETE FROM employee WHERE  name = %s'
+            delete_value = ('Stun',)
+            curs.execute(delete_script,delete_value)
 
 
-    conn.commit()
+            # view items in table each on it's own line
+            curs.execute('SELECT * FROM employee')
+            for record in curs.fetchall():
+                print(record[1], record[2])
+
+
+        conn.commit()
 except Exception as e:
     print(e)
 
 finally:
-    if curs is not None:
-        curs.close()
-
     if conn is not None:
         conn.close()
